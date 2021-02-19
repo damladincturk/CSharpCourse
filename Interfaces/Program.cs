f7fbc35fd1119a949d5bbd02829eb8fcee92bb4a
Damla Dibçtürk <damladnt97@gmail.com>
2021-02-19 21:53:13 +0300

    interface

---
+++ Interfaces/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+       }
+
+        interface IPerson {
+
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+        class Customer : IPerson
+        {
+            public int Id { get ; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Adress { get; set; }
+
+        }
+        class Student : IPerson
+        {
+            public int Id { get; set; }
+            public string FirstName{ get ; set; }
+            public string LastName { get; set; }
+            public string Department { get; set; }
+             
+        }
+    }
+}
