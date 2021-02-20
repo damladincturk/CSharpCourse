75099539979b4c0831f4de3b5f26d501f49d3d47
Damla Dibçtürk <damladnt97@gmail.com>
2021-02-20 21:17:18 +0300

    virtual

--- VirtualMethods/Program.cs
+++ VirtualMethods/Program.cs
@@ -6,7 +6,41 @@ namespace VirtualMethods
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SqlServer sqlServer = new SqlServer();
+            Mysql mysql = new Mysql();
+
+            sqlServer.Add();
+            mysql.Add();
         }
     }
+
+    class Database{
+
+        public virtual void Add() {
+
+            Console.WriteLine("Added by defaualt");
+        }
+        public void Delete() {
+
+             Console.WriteLine("Deleted by default");
+        }
+
+    }
+
+    class SqlServer : Database {
+
+        public override void Add()
+        {
+            Console.WriteLine("Added by Sql");
+            base.Add();
+        }
+
+    }
+
+    class Mysql :  Database
+    {
+
+
+    }
+
 }
