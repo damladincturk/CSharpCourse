982fa9e6634d3f27a77be2882036f41caef877c7
Damla Dibçtürk <damladnt97@gmail.com>
2021-02-20 21:36:45 +0300

    Abstract

---
+++ AbstractClacce/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AbstractClacce
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Database database = new SqlServer();
+            Database database1 = new OracleServer();
+
+            database.Add();
+            database1.Add();
+
+            database.Deleted();
+            database1.Deleted();
+        }
+    }
+
+    abstract class Database {
+
+        public void Add() {
+
+            Console.WriteLine("Added");
+
+        }
+
+        public abstract void Deleted();
+
+
+    }
+
+    class SqlServer : Database
+    {
+        public override void Deleted()
+        {
+            Console.WriteLine("Deleted by Sql");
+        }
+    }
+
+    class OracleServer : Database
+    {
+        public override void Deleted()
+        {
+            Console.WriteLine("Deleted by Oracle");
+        }
+    }
+}
