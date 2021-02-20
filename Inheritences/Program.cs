27523f8cc15042a609dead2174bb43a02f523de8
Damla Dibçtürk <damladnt97@gmail.com>
2021-02-20 20:53:25 +0300

    Inheritances

---
+++ Inheritences/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Inheritances
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Person[] persons = new Person[3] { new Customer() { FirstName ="Damla"}, new Student() { FirstName="Hakan"}, new Person() { FirstName="Adem"} };
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+            
+        }
+
+        class Person {
+
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+        }
+
+        class Customer : Person {
+
+            public string City { get; set; }
+
+
+        }
+        class Student : Person
+        {
+
+            public string Depertman { get; set; }
+
+
+        }
+
+    }
+}
---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VirtualMethods
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
